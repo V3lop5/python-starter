1e195020af91bc2d3edb7014a144a3f33002b72a
Paule <44635962+V3lop5@users.noreply.github.com>
2021-10-10 18:30:19 +0200

    Reformatted files

--- bigfoot/__main__.py
+++ bigfoot/__main__.py
@@ -1,14 +1,15 @@
 """
-This is the main file of project package_name.
+This is the main file of project bigfoot.
 
 It is invoked when someone runs python like:
-    python -m package_name
+    python -m bigfoot
 """
-from .bigfoot import FootExample
+from bigfoot import FootExample
 
 
 def main():
     FootExample().print_text()
-  
+
+
 if __name__ == "__main__":
-    main()  
+    main()
--- bigfoot/bigfoot.py
+++ bigfoot/bigfoot.py
@@ -3,9 +3,10 @@ Short example of an application
 
 """
 
+
 class FootExample(object):
     def __init__(self):
-      self.text = "Hello World!"
-      
+        self.text = "Hello World!"
+
     def print_text(self):
-      print(self.text)  
+        print(self.text)
