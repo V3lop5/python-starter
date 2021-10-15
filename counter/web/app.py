a4737538d53a11b036eae1a1969472a92ea66f66
Paule <44635962+V3lop5@users.noreply.github.com>
2021-10-15 12:02:25 +0200

    Added logger example

--- counter/web/app.py
+++ counter/web/app.py
@@ -7,4 +7,5 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello():
     name = request.args.get("name", "World")
+    app.logger.info(f'Processing hello_word with name {escape(name)}')
     return f'Hello, {escape(name)}!'
